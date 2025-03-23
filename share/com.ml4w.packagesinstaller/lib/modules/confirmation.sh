e9cadec6a6b169b63eb0a2fb3c9ea79a52d79e8a
Stephan Raabe <mail@stephan-raabe.de>
2025-03-23 22:16:55 +0100

    Updates

--- share/com.ml4w.packagesinstaller/lib/modules/confirmation.sh
+++ share/com.ml4w.packagesinstaller/lib/modules/confirmation.sh
@@ -1,6 +1,8 @@
-if gum confirm "${pkginst_lang['confirm_installation']}"; then
-    echo
-else
-    _echo "${pkginst_lang['installation_cancelled']}"
-    exit
+if [ "$assumeyes" == "1" ]; then
+    if gum confirm "${pkginst_lang['confirm_installation']}"; then
+        echo
+    else
+        _echo "${pkginst_lang['installation_cancelled']}"
+        exit
+    fi
 fi

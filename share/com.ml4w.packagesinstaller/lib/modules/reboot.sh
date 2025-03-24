547c15fd441d83f5a01efef5880d0bf30845d262
Stephan Raabe <mail@stephan-raabe.de>
2025-03-24 10:39:45 +0100

    Updates

--- share/com.ml4w.packagesinstaller/lib/modules/reboot.sh
+++ share/com.ml4w.packagesinstaller/lib/modules/reboot.sh
@@ -1,6 +1,8 @@
 echo $echo_line
 echo "${pkginst_lang['installation_completed']}"
 echo $echo_line
-if gum confirm "${pkginst_lang['reboot_question']}"; then
-    reboot
+if [ "$assumeyes" == "1" ]; then
+    if gum confirm "${pkginst_lang['reboot_question']}"; then
+        reboot
+    fi
 fi
